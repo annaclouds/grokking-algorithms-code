b6e15e25834d3bb564a8dfd5d0da248d9ea3c2ae
annaclouds <napha@ymail.com>
2022-09-22 11:11:21 -0700

    created a function that stores the smallest value in an arry; and stores the index of the smallest value

---
+++ selectionSort.py
@@ -0,0 +1,5 @@
+# sort an array from smallest to largest
+
+def findSmallest(arr):
+    smallest = arr[0]   # stores the smallest value.
+    smallest_index = 0  # stores the index of the smallest value.
